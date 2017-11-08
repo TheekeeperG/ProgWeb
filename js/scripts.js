a6231892f45b48c41773c7c2fbf900cce16459c2
Madoka09 <shippuuga.zero@gmail.com>
2017-11-08 08:03:20 -0600

    Ayuda

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,14 @@ $(document).ready(function() {
     $('.slider').bxSlider({
         mode: 'fade',
         slideWidth: 1700,
+        
         captions: true,
         auto: true,
         stopAutoOnClick: true,
         keyboardEnabled: true
+        
+        /*AAAAAQQQQQQQQQQUUUUUUUUUUIIIIIIIIIIIII*///
+        
+        
     });
 });
